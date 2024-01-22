7111a51e53e99bf3b5e59838cbb2c0bc2d940572
Sirawich <Sirawich_Pin@cmu.ac.th>
2024-01-22 21:58:17 +0700

    login_popup

--- src/components/Profilebar.js
+++ src/components/Profilebar.js
@@ -1,33 +1,37 @@
-import React, { useState } from 'react';
-import './Profile.css';
-import logo from './Icon/logo.svg';
-import loginIcon from './Icon/login.svg'
-import registerIcon from './Icon/register.svg'
-import ProfileButton from './ProfileButton';
-import homeIcon from './Icon/home.svg';
-import explorIcon from './Icon/explor.svg';
+import React, { useState } from "react";
+import "./Profile.css";
+import logo from "./Icon/logo.svg";
+import loginIcon from "./Icon/login.svg";
+import registerIcon from "./Icon/register.svg";
+import ProfileButton from "./ProfileButton";
+import homeIcon from "./Icon/home.svg";
+import explorIcon from "./Icon/explor.svg";
+import LoginButton from "./LoginButton";
+import Popup_Login from "./Popup_Login";
 const ProfileBar = () => {
-  const [Login,setLogin] = useState(false)
+  const [Login, setLogin] = useState(false);
   return (
-    
-    <div className='profilebarBackground'>
-      <div className='profilebar' >
-        <img src={logo} className='logo' alt="logo" />
+    <div className="profilebarBackground">
+      <div className="profilebar">
+        <img src={logo} className="logo" alt="logo" />
         <>
-          {Login? <></>
-          :
-          <div>
-            <ProfileButton Detail='ลงชื่อเข้าใช้' Img={loginIcon}/>
-            <ProfileButton Detail='สมัครบัญชี' Img={registerIcon}/>
-            
-          </div>}
+          {Login ? (
+            <></>
+          ) : (
+            <div>
+              <ProfileButton Detail="ลงชื่อเข้าใช้" Img={loginIcon} />
+              <ProfileButton Detail="สมัครบัญชี" Img={registerIcon} />
+            </div>
+          )}
         </>
-        <ProfileButton Detail='หน้าแรก' Img={homeIcon}/>
-        <ProfileButton Detail='สำรวจ' Img={explorIcon}/>
-        <div className='copyright'>
+        <ProfileButton Detail="หน้าแรก" Img={homeIcon} />
+        <ProfileButton Detail="สำรวจ" Img={explorIcon} />
+        <LoginButton />
+        <div className="copyright">
           Copyright © 2023 ·
-          <br/>
-          CPExhange.com โครงงานวิชา Software Engineer 261361 2/66 ภาควิศวกรรมคอมพิวเตอร์ มหาวิทยาลัยเชียงใหม่
+          <br />
+          CPExhange.com โครงงานวิชา Software Engineer 261361 2/66
+          ภาควิศวกรรมคอมพิวเตอร์ มหาวิทยาลัยเชียงใหม่
         </div>
       </div>
     </div>
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,15 +1,14 @@
-import ProfileBar from '../components/Profilebar';
-import './Mainpage.css';
+import LoginButton from "../components/LoginButton";
+import ProfileBar from "../components/Profilebar";
+import "./Mainpage.css";
 
-
-function Homepage(){
-    return(
-        <div className='mainbody'>
-            
-            <ProfileBar/>
-            ewfewfewfdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
-        </div>
-    )
+function Homepage() {
+  return (
+    <div className="mainbody">
+      <ProfileBar />
+      ewfewfewfdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
+    </div>
+  );
 }
 
 export default Homepage;
