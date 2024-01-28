02bdfecd8265f8b9ad60e34b5679315b6272aadc
nunu111 <nunu111za@hotmail.com>
2024-01-29 01:03:46 +0700

    Change postlist can use map function

--- src/components/Postbox.js
+++ src/components/Postbox.js
@@ -1,31 +1,45 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import pinIcon from './Icon/pin.svg'
 import PostTag from './posttag'
 import commentIcon from './Icon/comment.svg'
 import heartIcon from './Icon/heart.svg'
+import verifyIcon from './Icon/verify.svg'
 import { Link, Route, Router } from 'react-router-dom'
-const Postbox =() => {
+const Postbox =(props) => {
+  const [Tags,setTag] = useState([])
   const [isVilified,setIsVilified] =useState(false)
+
+ useEffect(()=>{
+   setTag(props.taglist)
+ },[])
+
   return (
     <div className='Postbox'>
-      <span className='Title'>Postsamplewdfewgewrewgggggg</span>
+      <span className='Title'>{props.title}</span>
       <img src={pinIcon} alt='pin' className='bookmark'/>
-      
+      <p className='date'>{props.date}</p>
       <hr/>
-        <PostTag TagName={'โพสต์ล่าสุด'}/>
-        <PostTag TagName={'โพสต์ล่าสุด'}/>
+        {
+          Tags.map((Tag,i)=>{
+            return <PostTag TagName={Tag} key={i}/>
+          })
+        } 
         <br/>
         <p className='detail'>
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        {props.detail}
         </p>
+        <div className='transparent'></div>
+        <br/>
       
-      <span className='date'>when</span>
       {isVilified?<></>:<></>}
       <span className='CLV'>
+      {
+          props.isVerify?<span className='text'><img src={verifyIcon} alt='verify' className='verify'/>Answer Vilified</span> :<></>
+        }
         <img src={commentIcon} alt='comment' className='comment'/>
-        <span className='text'>10</span>
+        <span className='text'>{props.comment}</span>
         <img src={heartIcon} alt='heart' className='heart'/>
-        <span className='text'>10</span>
+        <span className='text'>{props.like}</span>
         <Link to="/Post" className='viewButton'>View</Link>
             
 
--- src/components/Postlist.js
+++ src/components/Postlist.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Postlist.css'
 import Postbox from './Postbox'
 const PostList = () => {
+  const [PostList,setPostList] = useState([])
+
   return (
     <div className='Mainbox'>
         <div className='Topicbox'>
@@ -10,6 +12,23 @@ const PostList = () => {
             <span  className='Text'> โพสต์ล่าสุด</span>
         </div>
         
+        <Postbox title="Postsamplewdfewgewrewggggg" 
+        taglist ={["เนื้อหา 1","เนื้อหา 2"]}
+        detail="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." 
+        date="when"
+        isVerify ={false}
+        comment="10"
+        like="10"
+        />
+        <Postbox title="อยากกินไก่จังนะครับ" 
+        taglist ={["ไก่ย่าง","เเล่นเกมที่บ้าน"]}
+        detail="โจทย์คือ อยากกินไก่จังนะครับยากมากเลยครับ" 
+        date="โพสต์เมื่อ 9 : 40 | 15 Dec 22 by Username77"
+        isVerify ={true}
+        comment="1000"
+        like="10"
+        />
+        {/* <Postbox/>
         <Postbox/>
         <Postbox/>
         <Postbox/>
@@ -18,9 +37,7 @@ const PostList = () => {
         <Postbox/>
         <Postbox/>
         <Postbox/>
-        <Postbox/>
-        <Postbox/>
-        <Postbox/>
+        <Postbox/> */}
 
     </div>
   )
