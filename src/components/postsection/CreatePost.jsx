4c391731c3b17861ad558c7ba894d7cd1b8f5559
Sirawich <Sirawich_Pin@cmu.ac.th>
2024-01-30 00:56:53 +0700

    editor

--- src/components/postsection/CreatePost.jsx
+++ src/components/postsection/CreatePost.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import TextField from "@mui/material/TextField";
+import { CKEditor, CKEditorContext } from "@ckeditor/ckeditor5-react";
+import { ClassicEditor } from "@ckeditor/ckeditor5-editor-classic";
+import { Context } from "@ckeditor/ckeditor5-core";
+import { Bold, Italic } from "@ckeditor/ckeditor5-basic-styles";
+import { Essentials } from "@ckeditor/ckeditor5-essentials";
+import { Paragraph } from "@ckeditor/ckeditor5-paragraph";
 
 export default function CreatePost(props) {
   return (
