7b2ec46fb243a60a884b9bccd029e41ecc8ad5e8
Sirawich <Sirawich_Pin@cmu.ac.th>
2024-01-31 15:05:09 +0700

    click enter to login

--- src/components/Popup_Login.js
+++ src/components/Popup_Login.js
@@ -6,6 +6,7 @@ import "../pages/Mainpage.css";
 import ReactDOM from "react-dom";
 import logo from "./Icon/logo.svg";
 import XIcon from "./Icon/X.svg";
+
 const Login = (props) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -15,7 +16,7 @@ const Login = (props) => {
 
   const navigate = useNavigate();
 
-  const onButtonClick = () => {
+  const onButtonClick = (event) => {
     // Set initial error values to empty
     setEmailError("");
     setPasswordError("");
@@ -42,65 +43,78 @@ const Login = (props) => {
     togglePopupVisibility();
   };
 
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      onButtonClick();
+    }
+  };
 
-  return isPopupVisible
-    ? ReactDOM.createPortal(
+  return isPopupVisible ? (
+    ReactDOM.createPortal(
       <div>
-        <div className="popup-overlay" >
-        <div style={{width: "100%", height: "100%"}} onClick={togglePopupVisibility}/> {/* Add this line to close the popup on click */}
-          <div className="popup-container" >
-              <div className={"mainContainer"}>
-                <div className={"titleContainer"}>
-                  <img src={logo} className="logo" alt="logo" />
-                  <div>logging in...</div>
-                  <button
-                    className="exitButton"
-                    onClick={() => {
-                      console.log("Exit button clicked!");
-                      togglePopupVisibility();
-                    }}
-                  >
-                    <img src={XIcon} className="ExitIcon" alt="Exit" />
-                  </button>
-                </div>
-                <br />
-                <div className={"inputContainer"}>
-                  Username
-                  <input
-                    value={email}
-                    placeholder="Enter your username here"
-                    onChange={(ev) => setEmail(ev.target.value)}
-                    className={"inputBox"}
-                  />
-                  <label className="errorLabel">{emailError}</label>
-                </div>
-                <br />
-                <div className={"inputContainer"}>
-                  Password
-                  <input
-                    value={password}
-                    placeholder="Enter your password here"
-                    onChange={(ev) => setPassword(ev.target.value)}
-                    className={"inputBox"}
-                  />
-                  <label className="errorLabel">{passwordError}</label>
-                </div>
-                <br />
-                <div className={"inputContainer"}>
-                  <input
-                    className={"inputButton"}
-                    type="button"
-                    onClick={onButtonClick}
-                    value={"Log in"}
-                  />
-                </div>
+        <div className="popup-overlay">
+          <div
+            style={{ width: "100%", height: "100%" }}
+            onClick={togglePopupVisibility}
+          />
+          <div className="popup-container">
+            <div className={"mainContainer"}>
+              <div className={"titleContainer"}>
+                <img src={logo} className="logo" alt="logo" />
+                <div>logging in...</div>
+                <button
+                  className="exitButton"
+                  onClick={() => {
+                    console.log("Exit button clicked!");
+                    togglePopupVisibility();
+                  }}
+                >
+                  <img src={XIcon} className="ExitIcon" alt="Exit" />
+                </button>
               </div>
+              <br />
+              <div className={"inputContainer"}>
+                Username
+                <input
+                  value={email}
+                  placeholder="Enter your username here"
+                  onChange={(ev) => setEmail(ev.target.value)}
+                  onKeyPress={handleKeyPress}
+                  className={"inputBox"}
+                />
+                <label className="errorLabel">{emailError}</label>
+              </div>
+              <br />
+              <div className={"inputContainer"}>
+                Password
+                <input
+                  value={password}
+                  placeholder="Enter your password here"
+                  onChange={(ev) => setPassword(ev.target.value)}
+                  onKeyPress={handleKeyPress}
+                  className={"inputBox"}
+                  type="password"
+                />
+                <label className="errorLabel">{passwordError}</label>
+              </div>
+              <br />
+              <div className={"inputContainer"}>
+                <input
+                  className={"inputButton"}
+                  type="button"
+                  onClick={onButtonClick}
+                  value={"Log in"}
+                />
+              </div>
+            </div>
           </div>
         </div>
-        </div>,
-        document.body
-      )
-    : <></>;
+      </div>,
+      document.body
+    )
+  ) : (
+    <></>
+  );
 };
 
 export default Login;
