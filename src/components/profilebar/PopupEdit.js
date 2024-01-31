2682da3df98ad5a2e8cf7c832be644484870e225
Sirawich <Sirawich_Pin@cmu.ac.th>
2024-02-01 00:06:08 +0700

    edit profile

--- src/components/profilebar/PopupEdit.js
+++ src/components/profilebar/PopupEdit.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import unknowIcon from "../Icon/unknow.svg";
 import XIcon from "../Icon/X.svg";
+import { SigninFunc } from "../function/Signinfunc";
 
 const PopupEdit = ({ visible, onClose, onEdit }) => {
+  const [newdisplayname, setNewdisplayname] = useState("");
+  const [newusername, setNewusername] = useState("");
+  const [newpassword, setNewpassword] = useState("");
+  const [newpassword1, setNewpassword1] = useState("");
+
   if (!visible) return null;
 
   return ReactDOM.createPortal(
@@ -20,21 +26,58 @@ const PopupEdit = ({ visible, onClose, onEdit }) => {
             <p className="text">Role : ผู้ใช้ทั่วไป</p>
             <p className="text">แก้ไข Profile</p>
           </div>
-          <div></div>
-          <div className="buttonContainer">
+          <div className="inputContainer">
+            <label>Displayname</label>
             <input
-              className="viewButton"
-              type="button"
-              onClick={onEdit}
-              value={"บันทึก"}
-            />
-            <img
-              src={XIcon}
-              alt="Exit"
-              className="exitButton1"
-              onClick={onClose}
+              value={newdisplayname}
+              placeholder="Enter your Displayname here"
+              onChange={(ev) => setNewdisplayname(ev.target.value)}
+              className="inputBox"
             />
           </div>
+          <div className="inputContainer">
+            <label>Username</label>
+            <input
+              value={newusername}
+              placeholder="Enter your new username here"
+              onChange={(ev) => setNewusername(ev.target.value)}
+              className="inputBox"
+            />
+          </div>
+          <div className="inputContainer">
+            <label>Password</label>
+            <input
+              value={newpassword}
+              placeholder="Enter your new password here"
+              onChange={(ev) => setNewpassword(ev.target.value)}
+              className="inputBox"
+              type="password"
+            />
+          </div>
+          <div className="inputContainer">
+            <label>Confirm Password</label>
+            <input
+              type="password"
+              value={newpassword1}
+              placeholder="Enter your Confirm new password here"
+              onChange={(ev) => setNewpassword1(ev.target.value)}
+              className="inputBox"
+            />
+          </div>
+
+          <input
+            className="viewButton"
+            type="button"
+            onClick={onEdit}
+            value="บันทึก"
+          />
+          <img
+            src={XIcon}
+            alt="Exit"
+            className="exitButton1"
+            style={{ top: "10px", right: "10px" }}
+            onClick={onClose}
+          />
         </div>
       </div>
     </div>,
