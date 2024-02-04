efcb682acdfcc51fc3b8c9bab38f2d862a20b2c4
nunu111 <nunu111za@hotmail.com>
2024-02-04 16:20:57 +0700

    add localhost IP

--- src/components/function/IPconfig.js
+++ src/components/function/IPconfig.js
@@ -2,7 +2,8 @@
 
 
 export function IPconfig() {
-    const IP="http://192.168.116.101:8080";
+    // const IP="http://192.168.116.101:8080";
+    const IP="http://localhost:8080";
     const getIP = () => {
         return IP
     }
