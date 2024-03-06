c9bbe5bb63656c439e8453f9945bd51e534ee45e
Sirawich <Sirawich_Pin@cmu.ac.th>
2024-03-07 01:49:21 +0700

    API edit profile

--- src/components/function/IPconfig.js
+++ src/components/function/IPconfig.js
@@ -1,13 +1,8 @@
-
-
-
 export function IPconfig() {
-    const IP="http://10.121.37.52:8080";
-    // const IP="http://localhost:8080";
-    const getIP = () => {
-        return IP
-    }
-  return {getIP}
-    
-  
+  const IP = "http://10.125.163.206:8080";
+  // const IP="http://localhost:8080";
+  const getIP = () => {
+    return IP;
+  };
+  return { getIP };
 }
